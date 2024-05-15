d2d6a01b4bc5525c32ad7033d31c2c5ba46dff48
GabrielPedroAurelioNogueira <158486301+GabrielPedroAurelioNogueira@users.noreply.github.com>
2024-05-15 19:52:24 +0100

    fundo 2

--- script/noticiais.js
+++ script/noticiais.js
@@ -1,20 +1,17 @@
 var icon=document.getElementById('icon')
 icon.addEventListener('click',show)
 var menu=document.getElementById('menu')
- 
+ var main=document.querySelectorAll('main')[0]
 let link=document.getElementsByClassName('item_menu')
 var main= document.getElementById('txt_main')
 function show(params) {
  if(menu.style.width=='130px'){
  
     menu.style.width='0px'
- 
- 
-    
+   
  }else{
     menu.style.width='130px'
  
-   
     
  }
 }
