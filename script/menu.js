58605e335abed8d1b2d8ce93ead09dbd66131b6c
Gabriel <gabrielpedroaurelio@gmail.com>
2024-10-16 06:34:45 +0100

    menu login resolvido

--- script/menu.js
+++ script/menu.js
@@ -1,1 +1,22 @@
-var menu_item= document.querySelectorAll('.item')
+var icon=document.getElementById('icon')
+icon.addEventListener('click',show)
+var menu=document.getElementById('menu')
+ var main=document.querySelectorAll('main')[0]
+let link=document.getElementsByClassName('item_menu')
+var main= document.getElementById('txt_main')
+var item_menu=document.querySelectorAll('a.item-menu')[0]
+ 
+function show(params) {
+ if(menu.style.width=='130px'){
+ 
+    menu.style.width='0px'
+   
+ }else{
+    menu.style.width='130px'
+ 
+    
+ }
+}
+function  fechar(params) {
+   menu.style.width='0px'
+}
